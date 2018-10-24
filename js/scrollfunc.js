5b9193663c7b7d2deb2e47cd3c724a801e538942
imoark <ryomakuumi@gmail.com>
2018-10-24 15:25:08 -0700

    add scrollspy

--- js/scrollfunc.js
+++ js/scrollfunc.js
@@ -12,17 +12,6 @@ $(document).ready(function(){
     clip: true, // If true, adjust scroll distance to prevent abrupt stops near the bottom of the page
     offset: 65,
     easing: 'easeInOutCubic', // Easing pattern to use
-    customEasing: function (time) {
-
-      // Function. Custom easing pattern
-      // If this is set to anything other than null, will override the easing option above
-
-      // return <your formulate with time as a multiplier>
-
-      // Example: easeInOut Quad
-      return time < 0.5 ? 2 * time * time : -1 + (4 - 2 * time) * time;
-
-    },
 
     // History
     updateURL: true, // Update the URL on scroll
